0849324f765f8abe82728727e5c6ee6a2087b8fc
Adithya <adithyaa.in@mouritech.com>
2021-03-24 16:55:48 +0530

    first commit

---
+++ MyNewAppGit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyNewAppGit
+//
+//  Created by Adithya on 24/03/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
