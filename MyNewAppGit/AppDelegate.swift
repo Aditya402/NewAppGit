ba6cbf053744b27bfd6fadef60ccfe21b14e7ae7
Adithya <adithyaa.in@mouritech.com>
2021-03-24 17:14:49 +0530

    pod added

--- MyNewAppGit/AppDelegate.swift
+++ MyNewAppGit/AppDelegate.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import CoreData
+import AFNetworking
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
--- MyNewAppGit/ViewController.swift
+++ MyNewAppGit/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import AFNetworking
 
 class ViewController: UIViewController {
 
